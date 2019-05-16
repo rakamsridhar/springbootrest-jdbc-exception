acb4de534cde3f3b68e996a3de0dfe1730810770
rakamsridhar <sridhar_ryakam@yahoo.com>
2019-05-16 11:16:44 +0530

    comment updates

--- src/main/java/com/fsd/web/springbootwebdemo/SpringbootwebdemoApplication.java
+++ src/main/java/com/fsd/web/springbootwebdemo/SpringbootwebdemoApplication.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class SpringbootwebdemoApplication {
-
+	// swagger documentation enabled
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootwebdemoApplication.class, args);
 	}
